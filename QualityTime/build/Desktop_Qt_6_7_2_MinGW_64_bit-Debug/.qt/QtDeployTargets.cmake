ef760a36318b2a9b6c1f29ff277737c28166b7b6
luigiplatania94 <luigiplatania94@gmail.com>
2024-06-25 16:43:38 +0200

    initial setup of Qt project

---
+++ QualityTime/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_QualityTime_FILE C:/Users/luigi/Desktop/QualityTime/QualityTime/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/QualityTime.exe)
+set(__QT_DEPLOY_TARGET_QualityTime_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_QualityTime_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
